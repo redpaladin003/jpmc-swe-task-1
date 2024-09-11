3889327fc391f63c805fbfe6659df978e06512b1
redpaladin003 <ag4299@drexel.edu>
2024-09-11 11:52:25 -0400

    jpmc-se-t1-fixed

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
+
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +9,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote),(quote["stock"], quote["top_bid"]["price"], quote["top_ask"]["price"], (quote["top_bid"]["price"] + quote["top_ask"]["price"])/2 ))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +18,25 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote),(quote["stock"], quote["top_bid"]["price"], quote["top_ask"]["price"], (quote["top_bid"]["price"] + quote["top_ask"]["price"])/2 ))
 
+  def test_getRatio_price_b_equals_to_0(self):
+    price_a = 119.5
+    price_b = 0.0
+    self.assertRaises(ZeroDivisionError, getRatio, price_a, price_b)
 
-  """ ------------ Add more unit tests ------------ """
+  def test_getRatio_normal_price(self):
+    price_a = 223.68
+    price_b = 112.56
+    result = price_a/price_b
+    self.assertEqual(getRatio(price_a, price_b), result)
 
+  def test_getRatio_negative_values(self):
+    price_a = -1
+    price_b = -2
+    print(getRatio(price_a, price_b))
+    self.assertEqual(getRatio(price_a, price_b), "Negative values not supported")
 
 
 if __name__ == '__main__':
